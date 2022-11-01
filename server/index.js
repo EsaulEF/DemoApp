274c5c9d05793b7ba1db707e748998f17df184cb
Esaul Esparza Flores <Esaul.EsparzaFlores@infobip.com>
2022-11-01 11:22:01 -0600

    remove console log

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ import {
   addReservation,
   getByEmail,
 } from "./database.js";
+import fs from 'fs';
+import crypto from 'crypto';
 
 const port = process.env.PORT || 3001;
 const app = express();
@@ -16,9 +18,37 @@ if (process.env.NODE_ENV === "production") {
 }
 createConnection();
 
+const signingSecretFile = process.cwd() + '/signingSecret.txt';
+let signingSecret = ''
+try {
+  fs.readFile(signingSecretFile, 'utf8', (err, data) => {
+    if (err) {
+      return;
+    }
+    signingSecret =  data;
+  });
+}catch (e) {
+  console.log(e)
+}
+
+const generateSignature  = (data , key) => {
+  let signature = ''
+  try{
+    signature = crypto.createHmac('sha256', key).update(data).digest("base64")
+  }catch (e) {
+
+  }
+  return signature;
+}
+
 app.use(express.json()); // to support JSON-encoded bodies
 
 app.get("/exchange/restaurant/reservations/:email", async (req, res) => {
+  const timestamp = req.headers['X-Ib-Exchange-Req-Timestamp'];
+  const signature = req.headers['X-Ib-Exchange-Req-Signature'];
+  const payload = req.body;
+
+ console.log(signature === generateSignature(timestamp+payload, signingSecret));
   const reservation = await getByEmail(req.params.email).catch((error) => {
     return res.status(500).json({ error });
   });
