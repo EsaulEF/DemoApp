32defc999537ab227ae33a1816e6a3b6e4922b4c
Esaul Esparza Flores <Esaul.EsparzaFlores@infobip.com>
2022-11-07 17:04:46 -0600

    remove console log

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ try {
 const generateSignature  = (data , key) => {
   let signature = ''
   try{
-    signature = CryptoJS.HmacSHA256(data, key);
+    signature = CryptoJS.HmacSHA256(data, key).toString("hex");
     //console.log(signature.digest().toString("hex"))
     //signature = CryptoJS.enc.Hex.stringify(signature);
     console.log(signature);
