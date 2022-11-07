50d3b6f341b68fc54352d4521e5d4da945bd62b6
Esaul Esparza Flores <Esaul.EsparzaFlores@infobip.com>
2022-11-07 15:01:19 -0600

    remove console log

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ import {
   addReservation,
   getByEmail,
 } from "./database.js";
-import fs from 'fs';
-import {CryptoJS} from 'crypto-js';
+import fs from 'fs'
+import HmacSHA256 from 'crypto-js/hmac-sha256';
 
 const port = process.env.PORT || 3001;
 const app = express();
@@ -34,9 +34,9 @@ try {
 const generateSignature  = (data , key) => {
   let signature = ''
   try{
-    signature = CryptoJS.HmacSHA256(data, key);
-    console.log(signature.digest().toString("hex"))
-    signature = CryptoJS.enc.Hex.stringify(signature);
+    signature = HmacSHA256(data, key);
+    console.log(signature)
+    signature = CryptoJS.enc.Hex.stringify(signature)
     console.log(signature);
   }catch (e) {
 
